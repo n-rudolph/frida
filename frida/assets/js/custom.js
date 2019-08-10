12c32724ef58f6578bcedbff917e74877b200432
Nicolas Rudolph <nicolas.rudolph2@gmail.com>
2019-08-10 16:39:50 -0300

    javascript to load images and modal added

---
+++ frida/assets/js/custom.js
@@ -0,0 +1,34 @@
+var totalAmountOfPictures = images.length;
+
+function loadPictures(maxImagesToLoad) {
+    var lastPictureIndexShown = $('.my-work-image').length;
+    var amountOfImagesLeft = totalAmountOfPictures - lastPictureIndexShown;
+    var imagesToLoad = amountOfImagesLeft < maxImagesToLoad ? amountOfImagesLeft : maxImagesToLoad;
+    var indexLimit = lastPictureIndexShown + imagesToLoad;
+    for (var i = lastPictureIndexShown; i < indexLimit; i++) {
+        var image = images[i];
+        var element = '<div class="col-4 my-work-image" style="cursor: pointer;"><span class="image fit"><img src="images/fulls/'+image.name+'" alt="" onclick="openModal('+i+')"/></span></div>';
+        $(element).insertBefore( $("#empty-spot") );
+    }
+    if ($('.my-work-image').length === totalAmountOfPictures) {
+        $('#load-more-button').hide();
+    }
+}
+
+function openModal(imageIndex) {
+    $('#modal-image').attr({
+        src: "images/fulls/"+images[imageIndex].name,
+        alt: images[imageIndex].name +": "+images[imageIndex].description
+    });
+    $('#image-description').text(images[imageIndex].description);
+    $('#image-modal').modal({
+        fadeDuration: 1000,
+        fadeDelay: 0.50
+    });
+}
+
+
+
+$(window).on('load', function() {
+   loadPictures(9);
+});
---
+++ frida/assets/js/images.js
@@ -0,0 +1,124 @@
+var images = [
+    {
+        "id": 1,
+        "name": "001.jpg",
+        "description": "pep",
+        "related": []
+    },
+    {
+        "id": 3,
+        "name": "003.jpg",
+        "description": "pep",
+        "related": []
+    },
+    {
+        "id": 4,
+        "name": "004.jpg",
+        "description": "pep",
+        "related": []
+    },
+    {
+        "id": 5,
+        "name": "005.jpg",
+        "description": "pep",
+        "related": []
+    },
+    {
+        "id": 6,
+        "name": "006.jpg",
+        "description": "pep",
+        "related": []
+    },
+    {
+        "id": 8,
+        "name": "008.jpg",
+        "description": "pep",
+        "related": []
+    },
+
+    {
+        "id": 9,
+        "name": "009.jpg",
+        "description": "pep",
+        "related": []
+    },
+    {
+        "id": 11,
+        "name": "011.jpg",
+        "description": "pep",
+        "related": []
+    },
+    {
+        "id": 12,
+        "name": "012.jpg",
+        "description": "pep",
+        "related": []
+    },
+
+    {
+        "id": 13,
+        "name": "013.jpg",
+        "description": "pep",
+        "related": []
+    },
+    {
+        "id": 14,
+        "name": "014.jpg",
+        "description": "pep",
+        "related": []
+    },
+    {
+        "id": 15,
+        "name": "015.jpg",
+        "description": "pep",
+        "related": []
+    },
+    {
+        "id": 16,
+        "name": "016.jpg",
+        "description": "pep",
+        "related": []
+    },
+
+    {
+        "id": 17,
+        "name": "017.jpg",
+        "description": "pep",
+        "related": []
+    },
+    {
+        "id": 18,
+        "name": "018.jpg",
+        "description": "pep",
+        "related": []
+    },
+    {
+        "id": 19,
+        "name": "019.jpg",
+        "description": "pep",
+        "related": []
+    },
+    {
+        "id": 20,
+        "name": "020.jpg",
+        "description": "pep",
+        "related": []
+    },
+    {
+        "id": 21,
+        "name": "021.jpg",
+        "description": "pep",
+        "related": []
+    },{
+        "id": 2,
+        "name": "002.jpg",
+        "description": "pep",
+        "related": []
+    },
+    {
+        "id": 7,
+        "name": "007.jpg",
+        "description": "pep",
+        "related": []
+    }
+];
